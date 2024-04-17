ece52b4a0d403692af6b0a51f365fd71be6dc064
Paulo Jefferson Mendes Oliveira <10948773+PauloJeff@users.noreply.github.com>
2024-04-17 12:43:21 -0300

    Create tabke Users

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,5 @@
 CREATE DATABASE IF NOT EXISTS queue;
+
+use queue;
+
+CREATE TABLE IF NOT EXISTS `Users`( `id` int NOT NULL AUTO_INCREMENT, `name` text, `email` text, `age` int DEFAULT NULL, `created_at` datetime NOT NULL, `updated_at` datetime NOT NULL, `deleted_at` datetime DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
